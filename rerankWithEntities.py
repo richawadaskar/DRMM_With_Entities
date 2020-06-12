c98361d6a6992a68fada41a7f444c9419ba5db05
Richa Wadaskar <richa_wadaskar@umail.ucsb.edu>
2020-06-11 23:18:08 -0700

    Tested additional linear combinations

--- rerankWithEntities.py
+++ rerankWithEntities.py
@@ -5,7 +5,14 @@ from operator import itemgetter
 word_weights = [0.90, 0.80, 0.70, 0.60, 0.50, 0.40, 0.30, 0.20, 0.10]
 entity_weights = [0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90]
 
-def combine_ranklists(word_weight, entity_weight, index_for_naming):
+word_weights2 = [0.69, 0.68, 0.67, 0.66, 0.65, 0.64, 0.63, 0.62, 0.61, 0.60]
+entity_weights2 = [0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.40]
+
+word_weights3 = [0.79, 0.78, 0.77, 0.76, 0.75, 0.74, 0.73, 0.72, 0.71, 0.70]
+entity_weights3 = [0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.30]
+
+
+def combine_ranklists(word_weight, entity_weight, index_for_naming, weight_vector):
     # read from 2 ranklists (DRMM Word and DRMM Entity)
     word_ranklist = open("DRMM-LCH-IDF-rob04-title.ranklist", "r")
     entity_ranklist = open("DRMM-Entity-rob04-title.ranklist", "r")
@@ -37,7 +44,7 @@ def combine_ranklists(word_weight, entity_weight, index_for_naming):
         combined_ranklist[query] = reranked
 
     # produce final ranklist for each query
-    combined_filename = "Combined_DRMM_title_" + str(index_for_naming) + ".ranklist"
+    combined_filename = "ranklists" + weight_vector + "/Combined_DRMM_title_" + str(index_for_naming) + ".ranklist"
     save_ranklist_file = open(combined_filename, "w")
 
     for itQ in combined_ranklist:
@@ -51,4 +58,6 @@ def combine_ranklists(word_weight, entity_weight, index_for_naming):
 
 
 for i in range(0, len(word_weights)):
-    combine_ranklists(word_weights[i], entity_weights[i], i)
+    combine_ranklists(word_weights[i], entity_weights[i], i, "1")
+    combine_ranklists(word_weights2[i], entity_weights2[i], i, "2")
+    combine_ranklists(word_weights3[i], entity_weights3[i], i, "3")
